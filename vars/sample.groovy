4a37ed9af6825fb8b316994596cfe9a5528d9bd6
Chordia <abhishek.chordia@nokia.com>
2021-06-20 09:09:03 +0530

    Initial Commit

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,9 +1,8 @@
 def call(name){
   if("${name}" == "abhishek")
-    println("Hello ${name}")
     log.info "Hello ${name}"
   else
-    println("Hi Human")
+    log.info "Hello ${name}"
   sleep 2
   echo "End of Script"
 }
